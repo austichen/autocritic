e086f2a20fc51914975a542775fa092b567ac20a
unknown <itsaustinchen@gmail.com>
2019-10-28 19:32:28 -0400

    use google custom search api

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
+import json
 import re
 from bs4 import BeautifulSoup
 from . import sent_analysis
+import os
+
+GOOGLE_API_KEY=os.environ['GOOGLE_API_KEY']
 
 
 def index(request):
@@ -16,28 +20,27 @@ def movie(request):
         if not movieTitle:
             response = JsonResponse({'message': 'Invalid request: title query invalid'}, status = 400)
             return response
-        res = requests.get('https://google.com/search?q=' + movieTitle + '%20site:rottentomatoes.com/m')
-        googleHtml = BeautifulSoup(res.text, 'html.parser')
-        searchResults = googleHtml.select('#search a')
+        
         link = None
-        for result in searchResults:
-            if 'https://www.rottentomatoes.com/m/' in result['href']:
-                link = result['href'].split('?q=')[1].split('&')[0]
-                if '/reviews' not in result['href']:
-                    link += '/reviews'
+        res = requests.get('https://www.googleapis.com/customsearch/v1?q='+movieTitle+'&cx=008963822358995440322:m2xfglpsusn&key='+GOOGLE_API_KEY)
+        res_json = json.loads(res.text)
+        search_result_items = res_json['items']
+        for result in search_result_items:
+            pageLink = result['link']
+            if re.match(r'https://www\.rottentomatoes\.com/m/([^/]*)$', pageLink):
+                link = pageLink + '/reviews'
                 break
         if link is None:
             response = JsonResponse({'message': 'Internal server error: Unable to find movie'}, status = 500)
             return response
-
         res = requests.get(link)
         rtHtml = BeautifulSoup(res.text, 'html.parser')
         res = requests.get(link.replace('/reviews', ''))
         rtHomeHtml = BeautifulSoup(res.text, 'html.parser')
-        totalPages = int(rtHtml.select('#reviews span.pageInfo')[0].text.split(' ').pop())
+        totalPages = int(rtHtml.select('span.pageInfo')[0].text.split(' ').pop())
         imageUrl = rtHtml.select('.panel-body.content_body img')[0]['src']
         movieTitle = rtHtml.select('.panel-body.content_body h2 > a')[0].text
-        reviews = rtHtml.select('#reviews .the_review')
+        reviews = rtHtml.select('.review_table .the_review')
         tomatometerScore = int(re.sub(r'[^0-9]', '', rtHomeHtml.select('h2.mop-ratings-wrap__score span.mop-ratings-wrap__percentage')[0].text))
 
         reviewList = []
